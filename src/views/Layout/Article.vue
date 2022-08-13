2d17e2b6eb5ea268b80b163d5247f8eafe65e265
pxy <13315155984@163.com>
2022-08-13 09:33:28 +0800

    面经信息铺设

--- src/views/Layout/Article.vue
+++ src/views/Layout/Article.vue
@@ -1,12 +1,139 @@
 <template>
-  <div>Article 二级路由组件</div>
+  <div class="article-page">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>面经后台</el-breadcrumb-item>
+      <el-breadcrumb-item>面经管理</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card shadow="never" border="false">
+      <template #header>
+        <div class="header">
+          <span>共 300 条记录</span>
+          <el-button icon="el-icon-plus" size="small" type="primary" round>
+            添加面经
+          </el-button>
+        </div>
+      </template>
+
+      <!-- body -->
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        :row-class-name="tableRowClassName"
+      >
+        <el-table-column prop="stem" label="标题" width="260">
+        </el-table-column>
+        <el-table-column prop="creator" label="作者"> </el-table-column>
+        <el-table-column prop="likeCount" label="点赞"> </el-table-column>
+        <el-table-column prop="views" label="浏览数"> </el-table-column>
+        <el-table-column prop="createdAt" label="更新时间"> </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'article-page',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      current: 1,
+      pageSize: 10
+    }
+  },
+  async created () {
+    let res = await this.$axios({
+      url: '/admin/interview/query',
+      params: {
+        current: this.current,
+        pageSize: this.pageSize
+      }
+    })
+    console.log(res)
+    this.tableData = res.data.data.rows
+  },
+  methods: {
+    tableRowClassName ({ row, rowIndex }) {
+      if (rowIndex % 2 == 0) {
+        return 'warning-row'
+      } else if (rowIndex % 2 != 0) {
+        return 'success-row'
+      }
+      return ''
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.el-card {
+  margin-top: 25px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 16px;
+  }
+  .actions {
+    font-size: 18px;
+    display: flex;
+    justify-content: space-around;
+    color: #666;
+    > i:hover {
+      color: rgba(114, 124, 245, 1);
+      cursor: pointer;
+    }
+  }
+}
+.el-pagination {
+  margin-top: 20px;
+  text-align: center;
+}
+.el-breadcrumb {
+  margin-top: 10px;
+}
+.el-form {
+  padding-right: 40px;
+}
+.quill-editor {
+  ::v-deep .ql-editor {
+    height: 300px;
+  }
+}
+.el-rate {
+  padding: 10px 0;
+}
+.article-preview {
+  padding: 0 40px 40px 40px;
+  > h5 {
+    font-size: 20px;
+    color: #666;
+    border-bottom: 1px dashed #ccc;
+    padding-bottom: 30px;
+    margin: 0 0 20px 0;
+  }
+}
+::v-deep .el-table .warning-row {
+  background: oldlace;
+}
+::v-deep .el-table .success-row {
+  background: #f0f9eb;
+}
 </style>
